ae659fb9e3c38c2465287d6a76cd14bd43bedd0b
Алексей Дзюба <95968263+Dzuba110729@users.noreply.github.com>
2022-03-12 22:35:49 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.header-burger').click(function(event) {
+        $('.header-burger, .menu').toggleClass('active');
+    });
+    $('.header-burger1').click(function(event) {
+        $('.header-burger1, .menu1').toggleClass('active');
+    });
+});
